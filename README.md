7bc7bf541e0b777ff7a9ad68494a36b40b4325c1
JuanPedrazaD <juan_pedraza@cun.edu.co>
2024-11-18 22:46:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+composer install
+php artisan migrate
+php artisan db:seed
+
+Variables de entorno para probar en local
+APP_NAME=Laravel
+APP_ENV=local
+APP_KEY=base64:zPQMeP7FN3qMgJDYrkZM2GCrybsG1HXd9MMbSX1K7n8=
+APP_DEBUG=true
+APP_TIMEZONE=UTC
+APP_URL=http://localhost
+
+APP_LOCALE=en
+APP_FALLBACK_LOCALE=en
+APP_FAKER_LOCALE=en_US
+
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=ecommercevue
+DB_USERNAME=root
+DB_PASSWORD=
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word, // Genera una palabra aleatoria como nombre
+            'price' => $this->faker->randomFloat(0, 1000, 10000), // Genera un precio aleatorio entre 1 y 100 con 2 decimales
+            'description' => $this->faker->sentence, // Genera una frase aleatoria como descripción
+        ];
+    }
+}
